a9a4484b272062ae727f3d9fb1b4d024c845451f
likeeyong17@gmail.com <likeeyong17@gmail.com>
2025-07-01 04:14:58 +0000

    Created Login and Signup; Edited settings; Deleted Form1

---
+++ client_code/Login/__init__.py
@@ -0,0 +1,26 @@
+from ._anvil_designer import LoginTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+class Login(LoginTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def login_btn_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # get and format the username
+        email_for_login = f"{self.item['username']}@nursery.com"
+        # attempt a log in
+        try:
+            anvil.users.login_with_email(email_for_login_password)
+            # if login is successful, open with main app form
+            anvil.open_form('MainDashboard')
+        except anvil.sers.AuthenticationFailed as e:
+            alert(str(e))
+        
